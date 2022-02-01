d31d0726df4a7d4843254372c9738f0b375a89e6
Sagar Kapoor <sagrkapr@gmail.com>
2022-02-01 08:46:29 +0200

    Renewed effort for TOP

--- game.js
+++ game.js
@@ -13,27 +13,35 @@ function playSingleRound() {
     let playerSelection = "rock";
 
     if (playerSelection === computerSelection) {
+        console.log("It was a tie");
         return "It was a tie";    
     }
     else if (playerSelection === "rock" && computerSelection === "paper") {
+        console.log("Computer wins");
         return "Computer wins";
     }
     else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "Player wins"
+        console.log("Player wins");
+        return "Player wins";
     }
     else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log("Player wins");
         return "Player wins";
     }
     else if (playerSelection === "paper" && computerSelection === "scissors") {
+        console.log("Computer wins");
         return "Computer wins";
     }
     else if (playerSelection === "scissors" && computerSelection === "rock") {
+        console.log("Computer wins");
         return "Computer wins";
     }
     else if (playerSelection === "scissors" && computerSelection === "paper") {
+        console.log("Player wins");
         return "Player wins";
     }
     else {
+        console.log("Incorrect Input");
         return "Incorrect Input";
     }
 }
